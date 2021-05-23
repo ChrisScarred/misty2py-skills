784400d9756602435692f6c2f32b1cd67c618dee
ChrisScarred <scarred.chris@gmail.com>
2021-05-23 22:50:15 +0200

    feat(question_answering): speaking

--- misty2py_skills/question_answering.py
+++ misty2py_skills/question_answering.py
@@ -1,14 +1,10 @@
-# TODO
-# CaptureSpeech api/audio/speech/capture
-# emits VoiceRecord event
-# speech_transcripter
-# converse.speak
-
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from misty2py.utils.base64 import *
 from pymitter import EventEmitter
 from misty2py.utils.generators import get_random_string
 import os
+import datetime
+from num2words import num2words
 
 from misty2py_skills.utils.utils import get_misty, get_abs_path, get_files_in_dir, get_base_fname_without_ext, get_wit_ai_key
 from misty2py_skills.utils.status import Status, ActionLog
@@ -20,7 +16,7 @@ ee = EventEmitter()
 misty = get_misty()
 status = Status()
 action_log = ActionLog()
-speech_event = "speech_" + get_random_string(6)
+user_speech_event = "user_speech_" + get_random_string(6)
 speech_transcripter = SpeechTranscripter(get_wit_ai_key())
 
 SAVE_DIR = get_abs_path("data")
@@ -43,14 +39,14 @@ def _get_audio_names() -> List[str]:
     return audio_list
 
 
-@ee.on(speech_event)
+@ee.on(user_speech_event)
 def listener(data: Dict):   
     if data.get("errorCode", -1) == 0:
         status.set_(status="inference")
 
     if data.get("errorCode", -1) == 3:
         status.set_(status="reinit")
-        
+    
 
 def speech_capture_init() -> None:
     capture_speech = misty.perform_action("speech_capture", data={"RequireKeyPhrase": False})
@@ -75,24 +71,103 @@ def perform_inference() -> None:
             )
             speech_wav = speech_transcripter.load_wav(f_name)
             speech_text = speech_transcripter.audio_to_text(speech_wav, show_all=True)
-            print(speech_text)
-    status.set_(status="stop")
+            status.set_(status="prepare_reply", data=speech_text)
+        else:
+            status.set_(status="reinit")
+    else:
+        status.set_(status="reinit")
 
 
-def speech_capture() -> None:
-    subscribe_voice_record = misty.event("subscribe", type = "VoiceRecord", name = speech_event, event_emitter = ee)
-    action_log.append_({"subscribe_voice_record": subscribe_voice_record})
-        
-    speech_capture_init()
+def _get_intents_keywords(entities: Dict) -> Tuple[List[str], List[str]]:
+    intents = []
+    keywords = []
+    for key, val in entities.items():
+        if key == "intent":
+            intents.extend([dct.get("value") for dct in val])
+        else:
+            keywords.append(key)
+    return intents, keywords
 
-    while status.get("status") != "stop":
-        if status.get("status") == "reinit":
-            speech_capture_init()
-        if status.get("status") == "inference":
-            perform_inference()
 
-    unsubscribe_voice_record = misty.event("unsubscribe", name = speech_event)
-    action_log.append_({"unsubscribe_voice_record": unsubscribe_voice_record})
+def choose_reply() -> None:
+    data = status.get("data").get("content", {})    
+    intents, keywords = _get_intents_keywords(data.get("entities"))
+    
+    if "greet" in intents:
+        if "hello" in keywords:
+            status.set_(status="ready_to_speak", data="hello")
+        elif "goodbye" in keywords:
+            status.set_(status="ready_to_speak", data="goodbye")
+        else:
+            status.set_(status="ready_to_speak", data="hello")
+    
+    elif "datetime" in intents:
+        if "date" in keywords:
+            status.set_(status="ready_to_speak", data="date")
+        elif "month" in keywords:
+            status.set_(status="ready_to_speak", data="month")
+        elif "year" in keywords:
+            status.set_(status="ready_to_speak", data="year")
+        else:
+            status.set_(status="ready_to_speak", data="unknown")
+    
+    elif "test" in intents:
+        status.set_(status="ready_to_speak", data="test")
+    
+    else:
+        status.set_(status="ready_to_speak", data="unknown")
+
+
+def speak(utterance: str) -> None:
+    print(utterance)
+
+    speaking = misty.perform_action(
+        "speak",
+        data={
+            "Text": utterance, 
+            "Flush": "true"
+        },
+    )
+    action_log.append_({"speaking": speaking})
+    
+    if status.get("data") == "goodbye":
+        status.set_(status="stop")
+    else:
+        status.set_(status="reinit")
+
+
+def perform_reply() -> None:
+    _type = status.get("data")
+    
+    if _type == "test":
+        speak("I received your test.")
+    
+    elif _type == "unknown":
+        speak("I am sorry, I do not understand.")
+    
+    elif _type == "hello":
+        speak("Hello!")
+    
+    elif _type == "goodbye":
+        speak("Goodbye!")
+    
+    elif _type == "year":
+        now = datetime.datetime.now()
+        speak("It is the year %s." % now.year)
+    
+    elif _type == "month":
+        now = datetime.datetime.now()
+        speak("It is the month of %s." % now.strftime("%B"))
+    
+    elif _type == "date":
+        now = datetime.datetime.now()
+        speak(             
+            "It is the %s of %s, year %s." % (
+                num2words(now.day, to='ordinal'), 
+                now.strftime("%B"), 
+                now.year
+            )
+        )
 
 
 def question_answering() -> Dict:
@@ -108,7 +183,34 @@ def question_answering() -> Dict:
     set_volume = misty.perform_action("volume_settings", data="low_volume")
     action_log.append_({"set_volume": set_volume})
 
-    speech_capture()
+    subscribe_voice_record = misty.event("subscribe", type = "VoiceRecord", name = user_speech_event, event_emitter = ee)
+    action_log.append_({"subscribe_voice_record": subscribe_voice_record})
+    
+    print("Listening")
+    speech_capture_init()
+
+    while status.get("status") != "stop":
+        current_status = status.get("status")
+
+        if current_status == "reinit":
+            status.set_(status="initialised")
+            print("Listening")
+            speech_capture_init()
+
+        elif current_status == "inference":
+            print("Analysing")
+            perform_inference()
+
+        elif current_status == "prepare_reply":
+            print("Preparing the reply")
+            choose_reply()
+
+        elif current_status == "ready_to_speak":
+            print("Replying")
+            perform_reply()
+
+    unsubscribe_voice_record = misty.event("unsubscribe", name = user_speech_event)
+    action_log.append_({"unsubscribe_voice_record": unsubscribe_voice_record})
 
     return success_parser_from_list(action_log.get_())
 
