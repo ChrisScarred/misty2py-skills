8e2734164bc38c42e7107117c3e5542cd6288ac6
ChrisScarred <scarred.chris@gmail.com>
2021-06-15 22:37:50 +0200

    test(): adjusted unit tests

--- tests/test_misty2py_skills.py
+++ tests/test_misty2py_skills.py
@@ -1,7 +1,9 @@
-def test_angry_expression(capsys):
-    from misty2py_skills.expressions.angry import angry_expression
-    from misty2py_skills.utils.utils import get_misty
+from misty2py.utils.utils import get_misty
 
+
+def test_angry_expression(capsys):
+    from misty2py_skills.expressions import angry_expression
+    
     with capsys.disabled():
         result = angry_expression(get_misty())
         print(result)
@@ -10,7 +12,6 @@ def test_angry_expression(capsys):
 
 def test_battery_printer(capsys):
     from misty2py_skills.demonstrations.battery_printer import battery_printer
-    from misty2py_skills.utils.utils import get_misty
 
     with capsys.disabled():
         result = battery_printer(get_misty(), 2)
@@ -29,7 +30,6 @@ def test_explore(capsys):
 
 def test_face_recognition(capsys):
     from misty2py_skills import face_recognition
-    from misty2py_skills.utils.utils import get_misty
 
     with capsys.disabled():
         result = face_recognition.face_recognition(get_misty())
@@ -38,8 +38,7 @@ def test_face_recognition(capsys):
 
 
 def test_free_memory(capsys):
-    from misty2py_skills.essentials.free_memory import free_memory
-    from misty2py_skills.utils.utils import get_misty
+    from misty2py.basic_skills.free_memory import free_memory
 
     with capsys.disabled():
         result = free_memory(get_misty(), "data")
@@ -66,43 +65,11 @@ def test_remote_control(capsys):
 
 
 def test_listening_expression(capsys):
-    from misty2py_skills.expressions.listening import listening_expression
-    from misty2py_skills.utils.utils import get_misty
+    from misty2py_skills.expressions import listening_expression
 
     with capsys.disabled():
         result = listening_expression(get_misty())
         print(result)
-        assert result.get("overall_success")
-
-
-def test_speech_transcripter(capsys):
-    from misty2py_skills.utils.utils import (
-        get_abs_path,
-        get_wit_ai_key,
-        get_files_in_dir,
-        get_base_fname_without_ext,
-    )
-    from misty2py_skills.essentials.speech_transcripter import SpeechTranscripter
-    from misty2py_skills.utils.converse import success_parser_from_list
-
-    potential_audios = get_files_in_dir(get_abs_path("tests/data"))
-    speech_transcripter = SpeechTranscripter(get_wit_ai_key())
-    results = []
-    for potential_audio in potential_audios:
-        if potential_audio.endswith(".wav"):
-            audio = speech_transcripter.load_wav(potential_audio)
-            results.append(
-                {
-                    get_base_fname_without_ext(
-                        potential_audio
-                    ): speech_transcripter.audio_to_text(audio)
-                }
-            )
-
-    with capsys.disabled():
-        result = success_parser_from_list(results)
-        print(result)
-        # note that here, the speech transcription is considered successful if it is performed; not when it is accurate
         assert result.get("overall_success")
 
 
